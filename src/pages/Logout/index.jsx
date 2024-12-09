001613488f8a4cafbe0aa2174e8714b1bf040519
Nitish814112 <n814112@gmail.com>
2024-12-09 13:30:30 +0530

    close #44

--- src/pages/Logout/index.jsx
+++ src/pages/Logout/index.jsx
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuth } from "../../store/auth";
 import logoutUserApi from "./api";
 import { useNavigate } from "react-router-dom";
-import { useToast } from "../../components/ToastContext"; 
+import { useToast } from "../../components/ToastContext";
 
 function Logout() {
   const { logout, state } = useAuth();
   const navigate = useNavigate();
   const { addToast } = useToast(); 
+  const hasLoggedOut = useRef(false); //using ref hook to check logout state
 
   useEffect(() => {
     const logoutUser = async () => {
+      if (hasLoggedOut.current) return; // if it is true it simply returns ;
+      hasLoggedOut.current = true;
+
       try {
         const result = await logoutUserApi();
         if (result.isSuccess) {
-          addToast("Logged out successfully!", "success"); 
+          addToast("Logged out successfully!", "success");
         } else {
           addToast(result.message || "Logout failed. Please try again.", "error"); 
         }
